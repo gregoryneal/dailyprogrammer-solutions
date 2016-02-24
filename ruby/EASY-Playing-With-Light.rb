48f3dcac302dc996ab7a18c414a7bd1a00a045a9
gregoryneal <gregcraig23@gmail.com>
2016-02-24 02:20:11 -0600

    optimized solution, includes bonus

--- ruby/EASY-Playing-With-Light.rb
+++ ruby/EASY-Playing-With-Light.rb
@@ -3,33 +3,26 @@ require 'set'
 
 start = Time.now
 
-input = ARGV[0] || "input.txt"
-file = File.open(input,'r')
+input = ARGV[0] || "test.txt"
 
-lines = file.readlines
-#get the number of bulbs
-number_of_bulbs = lines[0]
-byte_string = '0'*number_of_bulbs.to_i
-lines.delete_at(0)
-
-puts Time.now - start
-
-range = Set.new()
-i = 0
-lines.each do |line|
-
-	p = Time.now
-
-	line = line.split.sort.map(&:to_i)
-	new_range = Set.new((line.first..line.last))
-	range = range ^ new_range
-
-	puts "#{i}: \t #{Time.now - p}"
-
-	i += 1
+changes = SortedSet.new()
+File.open(input,'r').readlines[1..-1].each do |line|
+	line = line.split.map(&:to_i)
+	line.sort!
+	a = line.first
+	b = line.last+1
+	if(changes.include?(a))
+		changes.delete(a)
+	else
+		changes.add(a)
+	end
+	if(changes.include?(b))
+		changes.delete(b)
+	else
+		changes.add(b)
+	end
 end
 
-file.close
-
-puts range.length
+changes = changes.to_a
+puts changes.each_slice(2).map(&:last).reduce(:+) - changes.each_slice(2).map(&:first).reduce(:+)
 puts Time.now - start
